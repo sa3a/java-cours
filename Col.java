dff1220bb54606cd13ee98d9b223c4c6567e64e7
Sabuhi <sabuhi-amirov@mail.ru>
2017-09-17 22:02:59 +0500

    Java pr

---
+++ Col.java
@@ -0,0 +1,12 @@
+public class Col{
+
+public static void main(String[] args){
+
+System.out.println("Hello");
+int first=Integer.valueOf(args[0]);
+int second=Integer.valueOf(args[1]);
+int sum = first+second;
+System.out.println("Summa: "+sum);
+}
+
+}
